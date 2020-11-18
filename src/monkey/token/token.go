f1fa551cfff966a0449a6edee8b85efd0b69839f
tamakiii <tamakiii@users.noreply.github.com>
2020-11-19 01:05:30 +0900

    init token and lexer

---
+++ src/monkey/token/token.go
@@ -0,0 +1,35 @@
+package token
+
+type TokenType string
+
+type Token struct {
+  Type TokenType
+  Litelal string
+}
+
+const (
+  ILLEGAL = "ILLEGAL"
+  EOF = "EOF"
+
+  // Identifier + Literal
+  IDENT = "IDENT" // add, foobar, x, y, ...
+  INT = "INT" // 12345
+
+  // Calculator
+  ASSIGN = "="
+  PLUS = "+"
+
+  // Delimiter
+  COMMA = ","
+  SEMICOLON = ";"
+
+  // Parenthesis and Brace
+  LPAREN = "("
+  RPAREN = ")"
+  LBRACE = "{"
+  RBRACE = "}"
+
+  // Keyword
+  FUNCTION = "FUNCTION"
+  LET = "LET"
+)
