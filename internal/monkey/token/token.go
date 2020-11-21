d33a5bb1f700f788da54ee79333497d528adb606
tamakiii <tamakiii@users.noreply.github.com>
2020-11-21 13:35:18 +0900

    add some keywords and comparison ops

--- internal/monkey/token/token.go
+++ internal/monkey/token/token.go
@@ -38,11 +38,25 @@ const (
 	// Keyword
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
+
+	// Comparison Operators
+	EQ      = "=="
+	NOT_EQ  = "!="
 )
 
 var keywords = map[string]TokenType {
 	"fn": FUNCTION,
 	"let": LET,
+	"true": TRUE,
+	"false": FALSE,
+	"if": IF,
+	"else": ELSE,
+	"return": RETURN,
 }
 
 func LookupIdent(indent string) TokenType {
