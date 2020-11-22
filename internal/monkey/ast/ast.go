afd449084c80430a2e8e609c320b369fb52646fc
tamakiii <tamakiii@users.noreply.github.com>
2020-11-22 11:54:35 +0900

    fmt

--- internal/monkey/ast/ast.go
+++ internal/monkey/ast/ast.go
@@ -3,49 +3,48 @@ package ast
 import "github.com/tamakiii-sandbox/golang-interpreter/internal/monkey/token"
 
 type Node interface {
-  TokenLiteral() string
+	TokenLiteral() string
 }
 
 type Statement interface {
-  Node
-  statementNode()
+	Node
+	statementNode()
 }
 
 type Expression interface {
-  Node
-  expressionNode()
+	Node
+	expressionNode()
 }
 
 type Program struct {
-  Statements []Statement
+	Statements []Statement
 }
 
 type LetStatement struct {
-  Token token.Token // token.LET
-  Name *Identifier
-  Value Expression
+	Token token.Token // token.LET
+	Name  *Identifier
+	Value Expression
 }
 
 func (ls *LetStatement) statementNode() {}
 func (ls *LetStatement) TokenLiteral() string {
-  return ls.Token.Literal
+	return ls.Token.Literal
 }
 
 type Identifier struct {
-  Token token.Token // token.IDENT
-  Value string
+	Token token.Token // token.IDENT
+	Value string
 }
 
 func (i *Identifier) expressionNode() {}
 func (i *Identifier) TokenLiteral() string {
-  return i.Token.Literal
+	return i.Token.Literal
 }
 
 func (p *Program) TokenLiteral() string {
-  if len(p.Statements) > 0 {
-    return p.Statements[0].TokenLiteral()
-  } else {
-    return ""
-  }
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
 }
-
