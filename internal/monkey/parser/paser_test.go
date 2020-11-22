0e4d8ec728340ed01f1a6fa5f00d54b0b09f0f81
tamakiii <tamakiii@users.noreply.github.com>
2020-11-22 11:54:17 +0900

    impl AST and parser

---
+++ internal/monkey/parser/paser_test.go
@@ -0,0 +1,87 @@
+package parser
+
+import (
+  "testing"
+	// "internal/monkey/ast"
+	// "internal/monkey/lexer"
+	"github.com/tamakiii-sandbox/golang-interpreter/internal/monkey/ast"
+	"github.com/tamakiii-sandbox/golang-interpreter/internal/monkey/lexer"
+	"github.com/tamakiii-sandbox/golang-interpreter/internal/monkey/parser"
+)
+
+func TestLetStatement(t *testing.T) {
+  input := `
+let x = 5;
+let y = 10;
+let foobar = 838383;
+`
+
+  l := lexer.New(input)
+  p := parser.New(l)
+
+  program := p.ParseProgram()
+  checkParseErrors(t, p)
+
+  if program == nil {
+    t.Fatalf("ParseProgram() returned nil")
+  }
+  if len(program.Statements) != 3 {
+    t.Fatalf("program.Statements does not contain 3 statements. got=%d", len(program.Statements))
+  }
+
+  tests := []struct {
+    expectedIdentifier string
+  } {
+    {"x"},
+    {"y"},
+    {"foobar"},
+  }
+
+  for i, tt := range tests {
+    stmt := program.Statements[i]
+    if !testLetStatement(t, stmt, tt.expectedIdentifier) {
+      return
+    }
+  }
+}
+
+func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
+  if s.TokenLiteral() != "let" {
+    t.Errorf("s.TokenLiteral not 'let'. got=%q", s.TokenLiteral())
+  }
+
+  letStmt, ok := s.(*ast.LetStatement)
+  if ! ok {
+    t.Errorf("s not *ast.LetStatement. got=%T", s)
+    return false
+  }
+
+  if letStmt.Name.Value != name {
+    t.Errorf("letStmt.Name.Value not '%s'. got=%s", name, letStmt.Name.Value)
+    return false
+  }
+
+  if letStmt.Name.TokenLiteral() != name {
+    t.Errorf("letStmt.Name.TokenLiteral() not '%s'. got=%s", name, letStmt.Name.TokenLiteral())
+    return false
+  }
+
+  return true
+}
+
+func checkParseErrors(t *testing.T, p *parser.Parser) {
+  errors := p.Errors()
+
+  if len(errors) == 0 {
+    return
+  }
+
+   t.Errorf("parse has %d errors", len(errors))
+
+   for _, msg := range errors {
+     t.Errorf("parse error: %q", msg)
+   }
+
+   t.FailNow();
+}
+
